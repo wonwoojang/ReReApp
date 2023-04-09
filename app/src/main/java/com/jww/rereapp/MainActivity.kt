a9c55122c56f4ba26f7809fa1d6c976d27b37b3e
bywkd <cd100go@naver.com>
2023-04-09 21:41:09 +0900

    Feat: 1_파이어 베이스 콘솔 연결 완료 - analytics 추가 - crashlytics 추가 - remote config 추가

--- app/src/main/java/com/jww/rereapp/MainActivity.kt
+++ app/src/main/java/com/jww/rereapp/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.jww.rereapp
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
 import com.jww.rereapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
