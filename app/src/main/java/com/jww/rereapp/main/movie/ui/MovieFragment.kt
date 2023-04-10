eaf3681e52caf070113b8f7b4003f5f23bd656d6
bywkd <cd100go@naver.com>
2023-04-10 21:34:46 +0900

    Feat: 9_영화 음악 화면 UI 추가 완료

--- app/src/main/java/com/jww/rereapp/main/movie/ui/MovieFragment.kt
+++ app/src/main/java/com/jww/rereapp/main/movie/ui/MovieFragment.kt
@@ -1,6 +1,27 @@
 package com.jww.rereapp.main.movie.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.jww.rereapp.base.BaseFragment
+import com.jww.rereapp.databinding.FragmentMovieBinding
 
-class MovieFragment:BaseFragment() {
+class MovieFragment : BaseFragment() {
+    private var _binding: FragmentMovieBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMovieBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
--- app/src/main/java/com/jww/rereapp/main/music/ui/MusicFragment.kt
+++ app/src/main/java/com/jww/rereapp/main/music/ui/MusicFragment.kt
@@ -1,6 +1,27 @@
 package com.jww.rereapp.main.music.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.jww.rereapp.base.BaseFragment
+import com.jww.rereapp.databinding.FragmentMusicBinding
 
-class MusicFragment:BaseFragment() {
+class MusicFragment : BaseFragment() {
+    private var _binding: FragmentMusicBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMusicBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
