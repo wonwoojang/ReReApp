54f6fa3c506272084403d094558c90be54508b98
bywkd <cd100go@naver.com>
2023-04-10 22:14:13 +0900

    Feat: 10_info 화면 UI 추가 완료

--- app/src/main/java/com/jww/rereapp/main/info/ui/InfoFragment.kt
+++ app/src/main/java/com/jww/rereapp/main/info/ui/InfoFragment.kt
@@ -1,6 +1,52 @@
 package com.jww.rereapp.main.info.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import com.jww.rereapp.base.BaseFragment
+import com.jww.rereapp.databinding.FragmentInfoBinding
 
 class InfoFragment : BaseFragment() {
+    private var _binding: FragmentInfoBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentInfoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bind()
+    }
+
+    private fun bind() {
+        binding.run {
+            body.run {
+                notice.setOnClickListener {
+                    showToast("구현 중인 기능입니다.")
+                }
+                faq.setOnClickListener {
+                    showToast("구현 중인 기능입니다.")
+                }
+                openLicense.setOnClickListener {
+                    showToast("구현 중인 기능입니다.")
+                }
+                helper.setOnClickListener {
+                    showToast("구현 중인 기능입니다.")
+                }
+            }
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
 }
