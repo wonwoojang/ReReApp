80695be6f1201a6f17949ab1d251f258dc15b104
bywkd <cd100go@naver.com>
2023-06-03 18:35:25 +0900

    version - 1.0.3(v4)

--- app/src/main/java/com/jww/rereapp/common/models/WebToon.kt
+++
@@ -1,3 +0,0 @@
-package com.jww.rereapp.common.models
-
-data class WebToon(val id: String)
--- app/src/main/java/com/jww/rereapp/itemModel/WebToonAdapterItem.kt
+++
@@ -1,5 +0,0 @@
-package com.jww.rereapp.itemModel
-
-import com.jww.rereapp.common.models.WebToon
-
-data class WebToonAdapterItem(val webToon: WebToon)
