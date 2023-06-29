9ef480742a55077f7ec416c9bdaba6f5ad90f5d6
bywkd <cd100go@naver.com>
2023-06-29 23:36:58 +0900

    Feat: 39_rere 메인 화면 이미지 추가 완료

--- README.md
+++ README.md
@@ -7,13 +7,28 @@
 - 안드로이드 최대 버전 13 (33)
 
 * 깃 커밋 룰
+
 - Feat = 새로운 할일
 - Fix = 버그 수정
 - Refactor = 개선
 - Modify = 버그는 아니고 기획 변경으로 인한 수정 사항
 
 * 깃 머지 관리
+
 - dev, release 커밋은 전체 내용 push
-- master 커밋은 Squash commit 으로 버전 네임으로  push 예) version - 1.0.0 
+- master 커밋은 Squash commit 으로 버전 표시 push 예) version - 1.0.0
 
 * DI - Koin
+
+* CI/CD
+
+- Github Action 사용
+- 배포 Firebase - App Distribution 사용
+
+* 영화 검색 api
+
+- kmdb(한국 영화 데이터 베이스 사용) - https://www.kmdb.or.kr/info/api/apiList?menuIndex=115
+
+* 책 검색 api
+
+- 네이버 검색 (책 사용) - https://developers.naver.com/products/service-api/search/search.md
--- app/src/main/java/com/jww/rereapp/main/rere/ui/ReReFragment.kt
+++ app/src/main/java/com/jww/rereapp/main/rere/ui/ReReFragment.kt
@@ -1,6 +1,23 @@
 package com.jww.rereapp.main.rere.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.jww.rereapp.base.BaseFragment
+import com.jww.rereapp.databinding.FragmentRereBinding
 
 class ReReFragment : BaseFragment() {
+    private var _binding: FragmentRereBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRereBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
